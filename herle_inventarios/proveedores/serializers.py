15e12f1ba51b32878a52fa50fb06140a56456c78
raultr <raultr@gmail.com>
2016-05-13 02:04:59 -0500

    Se agrego el campo de codigo

--- herle_inventarios/proveedores/serializers.py
+++ herle_inventarios/proveedores/serializers.py
@@ -4,4 +4,4 @@ from .models import Proveedor
 class ProveedorSerializer(serializers.ModelSerializer):
 		class Meta:
 			model = Proveedor
-			fields = ( 'id','codigo','nombre','calle','numero','cp','pais','estado','rfc','telefono','email','comentarios')
+			fields = ( 'id','codigo','nombre','calle','numero','colonia','cp','pais','estado','rfc','telefono','email','comentarios')
