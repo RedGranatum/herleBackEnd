42e638f6e14adfb6321b43a0704093a685d52624
raultr <raultr@gmail.com>
2016-06-09 00:32:56 -0500

    administrador para inventario

--- herle_inventarios/inventarios/admin.py
+++ herle_inventarios/inventarios/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Inventario
+					
+class InvetarioAdmin(admin.ModelAdmin):
+	list_display =("id","compra_detalle","invoice_compra","material","calibre","ancho","largo",
+					  "codigo_producto","num_rollo","peso_kg","peso_lb","transporte","pais",
+					  "precio_libra","factor","precio_dolar","factor_impuesto","con_comercializadora",
+					  "factor_kilos","valor_kilo_dolar","valor_tonelada_pesos","valor_kilo_persos",
+					  "valor_final_kilo_pesos","descripcion","comentarios",)
 
-# Register your models here.
+	search_fields = ('id','compra_detalle','pais') # Campos por los que se puede buscar, si son campos foraneos se usa campo__nomcampoforaneo
+	list_filter =('pais',)
+	
+admin.site.register(Inventario,InvetarioAdmin)
--- herle_inventarios/inventarios/serializers.py
+++ herle_inventarios/inventarios/serializers.py
@@ -1,1 +1,11 @@
-serializers.py
+from rest_framework import serializers
+from .models import Inventario
+
+class InventarioSerializer(serializers.ModelSerializer):		
+		class Meta:
+			model = Inventario
+			fields = ("id","compra_detalle","invoice_compra","material","calibre","ancho","largo",
+					  "codigo_producto","num_rollo","peso_kg","peso_lb","transporte","pais",
+					  "precio_libra","factor","precio_dolar","factor_impuesto","con_comercializadora",
+					  "factor_kilos","valor_kilo_dolar","valor_tonelada_pesos","valor_kilo_persos",
+					  "valor_final_kilo_pesos","descripcion","comentarios")
