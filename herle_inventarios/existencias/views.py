922a3e2f648604cb579f3dff43701937e874cef3
raultr <raultr@gmail.com>
2016-07-02 18:50:11 -0500

    consulta de existencias agrupada por rollo

--- herle_inventarios/existencias/views.py
+++ herle_inventarios/existencias/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render
 from django.db.models import Sum
+from django.db import connection
 from .models import Existencia
 
 # Create your views here.
@@ -19,9 +20,29 @@ class ExistenciaRollo(APIView):
 
 class ExistenciaAgrupada(APIView):
 	def get(self ,request):
-		resultado = Existencia.objects.values('num_rollo').annotate(entradas_kd=Sum('entrada_kg'),salidas_kg=Sum('salida_kg'),existencia_kg=Sum('entrada_kg')-Sum('salida_kg'))
+		cursor = connection.cursor()
+
+		consulta ="""
+			select exist.num_rollo as id, exist.num_rollo,inv.codigo_producto,inv.calibre,inv.ancho,
+			sum(exist.entrada_kg) as entradas_kg,sum(exist.salida_kg) as salidas_kg,
+			sum(exist.entrada_kg) - sum(exist.salida_kg) as existencia_kg
+			from existencias_existencia as exist
+			join inventarios_inventario as inv on exist.num_rollo = inv.num_rollo
+			group by exist.num_rollo,inv.codigo_producto,inv.calibre,inv.ancho
+			"""
+		cursor.execute(consulta)
+		#resultado= cursor.fetchall()
+		resultado = self.dictfetchall(cursor)
+		#resultado = Existencia.objects.values('num_rollo').annotate(entradas_kd=Sum('entrada_kg'),salidas_kg=Sum('salida_kg'),existencia_kg=Sum('entrada_kg')-Sum('salida_kg'))
 		return  Response(data=resultado, status=status.HTTP_201_CREATED)
 
+	def dictfetchall(self,cursor):
+		"Return all rows from a cursor as a dict"
+		columns = [col[0] for col in cursor.description]
+		return [
+			dict(zip(columns, row))
+			for row in cursor.fetchall()
+		]
 
 class ExistenciaAgrupadaNumRollo(APIView):
 	def get(self ,request,num_rollo):
