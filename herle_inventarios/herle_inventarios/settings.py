46737213be76af763e4779d6e5bd4846e97d16d3
raultr <raultr@gmail.com>
2016-09-25 21:07:19 -0500

    error del token

--- herle_inventarios/herle_inventarios/settings.py
+++ herle_inventarios/herle_inventarios/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
      'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
